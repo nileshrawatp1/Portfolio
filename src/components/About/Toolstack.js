c5062b0058c586f27bd536dd41d61a8ebaa24c0e
nileshrawatp1 <33788262+nileshrawatp1@users.noreply.github.com>
2025-01-22 14:55:54 +0530

    new tool added

--- src/components/About/Toolstack.js
+++ src/components/About/Toolstack.js
@@ -6,6 +6,7 @@ import {
   SiSlack,
   SiVercel,
   SiZoho,
+  SiVite
 } from "react-icons/si";
 import { FaLinux, FaSalesforce, FaGithub  } from "react-icons/fa";
 
@@ -30,19 +31,23 @@ function Toolstack() {
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <SiVercel />
-        <div style={{fontSize:'0.6rem', color:'rgb(155 126 172)'}}>VERCEL</div> 
+        <div style={{fontSize:'0.6rem', color:'rgb(155 126 172)'}}>VERCEL</div>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <FaGithub />
-        <div style={{fontSize:'0.6rem', color:'rgb(155 126 172)'}}>GITHUB</div> 
+        <div style={{fontSize:'0.6rem', color:'rgb(155 126 172)'}}>GITHUB</div>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <FaSalesforce />
-        <div style={{fontSize:'0.6rem', color:'rgb(155 126 172)'}}>SALESFORCE</div> 
+        <div style={{fontSize:'0.6rem', color:'rgb(155 126 172)'}}>SALESFORCE</div>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <SiZoho />
-        <div style={{fontSize:'0.6rem', color:'rgb(155 126 172)'}}>ZOHO</div> 
+        <div style={{fontSize:'0.6rem', color:'rgb(155 126 172)'}}>ZOHO</div>
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiVite />
+        <div style={{fontSize:'0.6rem', color:'rgb(155 126 172)'}}>Vite</div>
       </Col>
     </Row>
   );
