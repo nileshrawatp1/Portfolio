c0bd147a45c34742f5ee978c7bee34ea40e2b2ff
nileshrawatp1 <33788262+nileshrawatp1@users.noreply.github.com>
2025-04-20 14:44:17 +0530

    One More Type of skill added

--- src/components/Home/Type.js
+++ src/components/Home/Type.js
@@ -2,23 +2,24 @@ import React from "react";
 import Typewriter from "typewriter-effect";
 
 function Type() {
-  return (
-    <Typewriter
-      options={{
-        strings: [
-          "Frontend Developer",
-          "React App Development",
-          "Web Development",
-          "MERN Stack Development",
-          "Software Development",
-          "Freelancer",
-        ],
-        autoStart: true,
-        loop: true,
-        deleteSpeed: 50,
-      }}
-    />
-  );
+    return (
+        <Typewriter
+            options={{
+                strings: [
+                    "Software Engineer",
+                    "Frontend Developer",
+                    "React App Development",
+                    "Web Development",
+                    "MERN Stack Development",
+                    "Software Development",
+                    "Freelancer",
+                ],
+                autoStart: true,
+                loop: true,
+                deleteSpeed: 50,
+            }}
+        />
+    );
 }
 
 export default Type;
